fadd0f96b313c8ac87d2bf936339b8fcfaf439bb
AylimasMenz <nkerkech@gmail.com>
2023-12-07 19:38:34 +0100

    commit 4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello there ;)" << std::endl;
+    cout << "Ol Korect !" <<endl;
     return 0;
 }
