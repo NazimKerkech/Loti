86bfa9c74a13b5ef3b32eeea95fcc74747ce42d6
Thomy <thomy.cadinot@gmail.com>
2023-12-08 16:43:31 +0100

    test opencv et qt

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 #include <vector>
 #include "image.h"
 
+#include <QCoreApplication>
+#include <QImage>
+#include <QLabel>
+#include <QPixmap>
+#include <QVBoxLayout>
+#include <QMainWindow>
+#include <opencv2/opencv.hpp>
+#include <QApplication>
+
 using namespace std;
 
 const string LOTI_DIR(SOURCE_DIR);
 
-int main() {
+int main(int argc, char* argv[]) 
+{
     
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello there ;)" << std::endl;
@@ -27,5 +37,28 @@ int main() {
     Bibliotheque[0].afficheDescripteurs();
     Bibliotheque[1].afficheDescripteurs();
 
-    return 0;
+    QApplication a(argc, argv);
+    // Read an image using OpenCV
+    cv::Mat cvImage = cv::imread("../atlantis.jpg");
+
+    // Convert OpenCV image to QImage
+    QImage qtImage(cvImage.data, cvImage.cols, cvImage.rows, cvImage.step, QImage::Format_RGB888);
+
+    // Set up the main window
+    QMainWindow mainWindow;
+    QWidget* centralWidget = new QWidget(&mainWindow);
+    QVBoxLayout* layout = new QVBoxLayout(centralWidget);
+
+    // Create a QLabel to display the image
+    QLabel* label = new QLabel(centralWidget);
+    label->setPixmap(QPixmap::fromImage(qtImage));
+
+    // Add the QLabel to the layout
+    layout->addWidget(label);
+    mainWindow.setCentralWidget(centralWidget);
+
+    // Show the main window
+    mainWindow.show();
+
+    return a.exec();
 }
